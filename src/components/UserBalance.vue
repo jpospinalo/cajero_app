8172d5f2542c5f8d5be549b96172f7bf794b2f18
Juan Ospina <jpospinalo@unal.edu.co>
2020-12-10 09:37:16 -0500

    add cors

--- src/components/UserBalance.vue
+++ src/components/UserBalance.vue
@@ -17,7 +17,7 @@
         created: function(){
             this.username = this.$route.params.username
             let self = this
-            axios.get("http://127.0.0.1:8000/user/balance/" + this.username)
+            axios.get("https://cajero-api07.herokuapp.com/user/balance/" + this.username)
                 .then((result) => {
                     self.balance = result.data.balance
                 })
